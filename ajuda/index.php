89d1c23b8ff9266be8d9c33e82cab7f7316aaeaf
Juan Carlos Coral <govindasd@gmail.com>
2016-04-13 18:57:40 -0300

    Concertado o footer na página 'ajuda' e o código de monitoramento do Google Analytics.

--- ajuda/index.php
+++ ajuda/index.php
@@ -273,30 +273,30 @@
 
    
   <footer>
-       <div class="container">
+       	<div class="container">
                 <div class="row diarissima-site-map-wrap">
-	            <div class="footer-logo-wrap col-lg-3 col-md-3 col-sm-12 col-xs-12">
-		            <img src="<?php getBaseUrl();?>/img/diarissima-negativo.svg" alt="logo da diaríssima no rodapé"/>
-	            </div>
-	            <div class="footer-links col-lg-9 col-md-9 col-sm-12 col-xs-12">
-		            <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
-			            <a href="#" class="footer-link-big">Sobre</a><br>
-			            <a href="#" class="footer-link-small">A Diaríssima</a><br>
-			            <a href="../termos-de-uso" class="footer-link-small">Termos de Uso</a><br>
-			            <a href="#" class="footer-link-small">Políticas de Privacidade</a><br>
+		            <div class="footer-logo-wrap col-lg-3 col-md-3 col-sm-12 col-xs-12">
+			            <img src="../img/diarissima-negativo.svg" alt="logo da diaríssima no rodapé"/>
 		            </div>
-		            <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
-			            <a href="#" class="footer-link-big">Como funciona</a><br>
-			            <a href="#" class="footer-link-small">Entenda</a><br>
-			            <a href="../quero-contratar" class="footer-link-small">Quero contratar</a><br>
-			            <a href="../quero-prestar-servicos" class="footer-link-small">Quero prestar serviços</a><br>
-		            </div>
-		            <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
-			            <a href="#" class="footer-link-big">Fale com a gente</a><br>
-			            <a href="#" class="footer-link-small">Ajuda</a><br>
-			            <a href="#" class="footer-link-small">Contato</a><br>
-		            </div>
-	            </div>    
+		            <div class="footer-links col-lg-9 col-md-9 col-sm-12 col-xs-12">
+			            <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+				            <a href="#" class="footer-link-big">Sobre</a><br>
+				            <a href="#" class="footer-link-small">A Diaríssima</a><br>
+				            <a href="../termos-de-uso" class="footer-link-small">Termos de Uso</a><br>
+				            <a href="#" class="footer-link-small">Políticas de Privacidade</a><br>
+			            </div>
+			            <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+				            <a href="#" class="footer-link-big">Como funciona</a><br>
+				            <a href="#" class="footer-link-small">Entenda</a><br>
+				            <a href="../quero-contratar" class="footer-link-small">Quero contratar</a><br>
+				            <a href="../quero-prestar-servicos" class="footer-link-small">Quero prestar serviços</a><br>
+			            </div>
+			            <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+				            <a href="#" class="footer-link-big">Fale com a gente</a><br>
+				            <a href="#" class="footer-link-small">Ajuda</a><br>
+				            <a href="#" class="footer-link-small">Contato</a><br>
+			            </div>
+		            </div>    
 	            </div> 
             </div>
             <div class="social-m-footer-wrap row">
