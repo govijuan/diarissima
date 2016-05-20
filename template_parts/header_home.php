5d5ffefdaf08751408a1a7af56c435089b7740cf
Juan Carlos Coral <govindasd@gmail.com>
2016-05-20 14:50:04 -0300

    adicionando estilos completos e responsivo do banner principal da home com links para contratar e para ser contratado

--- template_parts/header_home.php
+++ template_parts/header_home.php
@@ -4,16 +4,16 @@
             <div class="intro-text">
 	            <div class="pre-txt">Encontre o profissional ideal</div>
 	            <h2 class="intro-heading">Encontre-se com Diaríssima</h2>
-	            <a href="#" class="contratar-wrap">
+	            <a href="/quero-contratar" class="contratar-wrap">
 		            <h3>Quero Contratar</h3>
-		            <span>clique e saiba como</span>
+		            <span class="hidden-xs">clique e saiba como</span>
 		            <div class="seta-img-wrap">
 		            	<img src="/img/seta-home-feat.svg" />
 		            </div>
 		        </a>
-	            <a href="#" class="prestar-serv">
+	            <a href="/quero-prestar-servicos" class="prestar-serv">
 		            <h3>Quero Prestar Serviços</h3>
-		            <span>clique e saiba como</span>
+		            <span class="hidden-xs">clique e saiba como</span>
 		            <div class="seta-img-wrap">
 		            	<img src="/img/seta-q-p-s-como-func.svg" />
 		            </div>  
