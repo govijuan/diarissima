90b243580bd890b6e0c772197c578b31ad0691c4
Juan Carlos Coral <juan.carlos@planet108.de>
2016-05-20 08:45:58 -0300

    Adicionado texto grande e pequeno para os potoes de contratar e ser contratado na home com estilos parciais

--- template_parts/header_home.php
+++ template_parts/header_home.php
@@ -5,8 +5,12 @@
 	            <div class="pre-txt">Encontre o profissional ideal</div>
 	            <h2 class="intro-heading">Encontre-se com Diaríssima</h2>
 	            <a href="#" class="contratar-wrap">
+		            <h3>Quero Contratar</h3>
+		            <span>clique e saiba como</span>
 		        </a>
 	            <a href="#" class="prestar-serv">
+		            <h3>Quero Prestar Serviços</h3>
+		            <span>clique e saiba como</span>
 		        </a>
             </div>
         </div>
