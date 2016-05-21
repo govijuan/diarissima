82450c1798832e5c7556b3bbdc0cc27d200fd680
Daniel Vinciguerra <daniel.vinciguerra@bivee.com.br>
2016-05-21 18:56:02 -0300

    Painel de links de redes sociais

--- template_parts/header_q_contratar.php
+++ template_parts/header_q_contratar.php
@@ -24,32 +24,33 @@
 	        <div class="q-c-foto-header-wrap col-lg-9 col-md-9 com-sm-12">
 	        	<img src="img/destaque-q-contratar.jpg" alt="Foto de pia de cocina com frutas e flores, tudo limpo">
         	</div>
-	     </div>
+	      </div>
         <div class="depoim-links-soc-wrap">
-	       <div class="container">
-		       <div class="links-soc-wrap col-lg-3 col-md-3 col-sm-12">
-			       <h4>Compartilhe</h4>
-			        <ul class="links-soc">
-				        <li><a href="https://twitter.com/Diarissima"><i class="fa fa-twitter"></i></a></li>
-						<li><a href="https://www.instagram.com/diarissima/"><i class="fa fa-instagram"></i></a></li>
-				        <li><a href="https://www.facebook.com/Diarissima"><i class="fa fa-facebook"></i></a></li>
-			        </ul>
-		        </div>
+	        <div class="container">
+            <div class="links-soc-wrap col-lg-3 col-md-3 col-sm-12">
+                <h4 style="margin: 60px 0px 30px; color: rgb(9, 173, 236); text-transform: uppercase;">Compartilhe</h4>
+                <ul class="links-soc">
+                  <li><a href="https://twitter.com/Diarissima"><i class="fa fa-twitter"></i></a></li>
+                  <li><a href="#"><i class="fa fa-linkedin-square"></i></a></li>
+                  <li><a href="https://www.instagram.com/diarissima/"><i class="fa fa-instagram"></i></a></li>
+                  <li><a href="https://www.facebook.com/Diarissima"><i class="fa fa-facebook"></i></a></li>
+                </ul>
+            </div>
             <div class="depoimentos-serv-contr col-lg-9 col-md-9 col-sm-12">
 			        <h2 style="font-family: &quot;Montserrat-Hairline&quot;,sans-serif; font-weight: 100; font-size: 38px;">Depoimentos</h2>
 			        <div class="depoimento col-lg-4 col-md-4 col-sm-12">
                 <img src="img/depoimentos/francisca-mello.png">
-				        <h4>Francisca Mello</h4>
+				        <h4 style="color: rgb(9, 173, 236);">Francisca Mello</h4>
 				        <p>"Excelente o serviço de faxina. Fiquei muito satisfeita com a qualidade e agilidade do serviço!"</p>
 			        </div>
 			        <div class="depoimento col-lg-4 col-md-4 col-sm-12">
 								<img src="img/depoimentos/katia-romao.png">
-				        <h4>katia lima Romão</h4>
+				        <h4 style="color: rgb(9, 173, 236);">Katia Lima Romão</h4>
 				        <p>"Muito obrigada pelo serviço. A rita é uma excelente profissional. fiquei muito feliz. Irei pedir sempre com vocês."</p>
 			        </div>
 			        <div class="depoimento col-lg-4 col-md-4 col-sm-12">
 								<img src="img/depoimentos/francisco-mello.png">
-				        <h4>Francisco Mello</h4>
+				        <h4 style="color: rgb(9, 173, 236);">Francisco Mello</h4>
 				        <p>"Ja estava cansado de pedir referências a amigos e me decepcionar. A diarista foi muito educada e eficiente. Recomendo"</p>
 			        </div>
 		        </div>
