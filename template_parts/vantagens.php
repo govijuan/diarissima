e8cb3cecb7b214f56a5419eedffd628a781c6bd9
Daniel Vinciguerra <daniel.vinciguerra@bivee.com.br>
2016-05-21 20:41:26 -0300

    Implementação da seção 'Vantagens'

--- template_parts/vantagens.php
+++ template_parts/vantagens.php
@@ -1,38 +1,44 @@
 <!-- Services Section -->
-    <section id="services">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12 text-center">
-                    <h2 class="section-heading">Vantagens</h2>
-                    <h3 class="section-subheading text-muted">Aqui os profissionais são empresários de si mesmos. Diaríssima incentiva o empreendedorismo ao construir confiança entre as pessoas</h3>
-                </div>
-            </div>
-            <div class="row text-center">
-                <div class="col-md-4">
-                    <span class="fa-stack fa-4x">
-                        <i class="fa fa-circle fa-stack-2x text-primary"></i>
-                        <i class="fa fa-usd fa-stack-1x fa-inverse"></i>
-                    </span>
-                    <h4 class="service-heading">É gratuito!</h4>
-                    <p class="text-muted">Use à vontade. O Diaríssima é totalmente gratuito e transparente, tanto para quem contrata quanto para quem presta serviços, sem intermediários e sem tarifas ocultas.</p>
-                </div>
-                <div class="col-md-4">
-                    <span class="fa-stack fa-4x">
-                        <i class="fa fa-circle fa-stack-2x text-primary"></i>
-                        <i class="fa fa-star-o fa-stack-1x fa-inverse"></i>
-                    </span>
-                    <h4 class="service-heading">Diferencie-se pelo seu talento</h4>
-                    <p class="text-muted">Profissionais e contratantes podem se diferenciar por mérito e referências, sendo também mutuamente avaliados.</p>
-                </div>
-                <div class="col-md-4">
-                    <span class="fa-stack fa-4x">
-                        <i class="fa fa-circle fa-stack-2x text-primary"></i>
-                        <i class="fa fa-search fa-stack-1x fa-inverse"></i>
-                    </span>
-                    <h4 class="service-heading">Encontre e agende num clique!</h4>
-                    <p class="text-muted">Filtros inteligentes ajudam a encontrar os profissionais mais próximos de você e a agenda permite ao profissional um melhor controle do seu tempo aumentando sua receita.
-                    </p>
-                </div>
+<section style="background: rgb(247, 247, 247) none repeat scroll 0% 0%; padding: 75px 0px 100px;" id="services">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <span style="text-transform: uppercase; color: rgb(9, 173, 236); font-size: 12px; font-weight: bold;">Mais Razões Para Usar</span>
+                <h2 style="font-family: &quot;Montserrat-Hairline&quot;,sans-serif; font-weight: 100; font-size: 46px;" class="section-heading">Vantagens</h2>
+                <h3 style="font-family: &quot;Montserrat-Light&quot;,sans-serif; color: rgb(153, 153, 153); font-style: normal;" class="section-subheading text-muted">Aqui os profissionais são empresários de si mesmos.<br> Diaríssima incentiva o empreendedorismo ao construir confiança entre as pessoas</h3>
             </div>
         </div>
-    </section>
+        <div class="row text-center">
+            <div class="col-md-4">
+                <span class="fa-stack fa-4x">
+                  <img style="width: 72px; height: 72px;" src="img/vantagens/ico-gratuito.svg">
+                </span>
+                <h4 style="font-family: &quot;Montserrat-UltraLight&quot;,sans-serif; font-weight: 100; font-size: 32px;" class="service-heading">É gratuito!</h4>
+                <p class="text-muted">
+                  Baixar e utilizar o Diaríssima é grátis. Você só paga pela contratação do
+                  profissional, diretamente para ele.
+                </p>
+            </div>
+            <div class="col-md-4">
+                <span class="fa-stack fa-4x">
+                  <img style="width: 72px; height: 72px;" src="img/vantagens/ico-certificado.svg">
+                </span>
+                <h4 style="font-family: &quot;Montserrat-UltraLight&quot;,sans-serif; font-weight: 100; font-size: 32px;" class="service-heading">Profissionais Certificados</h4>
+                <p class="text-muted">
+                  Realizamos uma triagem em nossos cadastros e só mantemos os profissionais
+                  que possuem certificação presencial.
+                </p>
+            </div>
+            <div class="col-md-4">
+                <span class="fa-stack fa-4x">
+                  <img style="width: 72px; height: 72px;" src="img/vantagens/ico-seguranca.svg">
+                </span>
+                <h4 style="font-family: &quot;Montserrat-UltraLight&quot;,sans-serif; font-weight: 100; font-size: 32px;" class="service-heading">Maior Segurança</h4>
+                <p class="text-muted">
+                  Para evitar fraudes de personalidade, garantimos sua segurança através de 
+                  reconhecimento biométrico facial por imagem.
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
