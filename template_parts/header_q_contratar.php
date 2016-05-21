da9c157dc31db40decf3a00d840d7c37567d85b0
Daniel Vinciguerra <daniel.vinciguerra@bivee.com.br>
2016-05-20 21:17:41 -0300

    Titulo + slider da pagina quero contratar [working in process]

--- template_parts/header_q_contratar.php
+++ template_parts/header_q_contratar.php
@@ -1,6 +1,24 @@
-<header class="q-c-header">
-        <div class="container q-c-main-title-container">
-	        <div class="txt-principal-wrap-q-c col-lg-3 col-md-3 col-sm-12">
+
+<header class="q-c-header" style="background: rgb(9, 173, 236) none repeat scroll 0% 0%;">
+        <div class="container-fluid">
+          <div style="top: 150px; margin-top: 150px;" class="row">
+            <div class="col-md-6">
+              <h2 style="">Quero contratar</h2>
+            </div>
+            <div style="background: #f7f7f7 none repeat scroll 0% 0%; min-height: 70px;" class="col-md-6">
+            </div>
+          </div>
+
+          <div style="background: #f7f7f7 none repeat scroll 0% 0%;" class="row">
+            <div class="col-md-offset-1 col-md-10 text-center" style="padding: 10px 0px 50px ! important; font-family: &quot;Montserrat Hairline&quot;;">
+              <img src="img/destaque-q-contratar.jpg" alt="Foto de pia de cocina com frutas e flores, tudo limpo" width="100%">
+            </div>
+          </div>
+        </div>
+
+
+        <div class="container q-c-main-title-container hide">
+          <div class="txt-principal-wrap-q-c col-lg-3 col-md-3 col-sm-12 hide">
 		        <h2>Encontre o melhor prestador!</h2>
 	        </div>
 	        <div class="q-c-foto-header-wrap col-lg-9 col-md-9 com-sm-12">
@@ -35,3 +53,4 @@
 	        </div>
         </div>
 </header>
+
